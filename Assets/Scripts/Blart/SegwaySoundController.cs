398fcca59fbe3877939f9b10b6473c62dae6c45f
Benjamin <bhum783@aucklanduni.ac.nz>
2025-06-16 00:21:50 +1200

    feat - Added vroom sound

---
+++ Assets/Scripts/Blart/SegwaySoundController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SegwaySoundController : MonoBehaviour
+{
+  public AudioSource source;
+  private float power = 0;
+  private float powerPerSecond = 1f;
+
+  void Start()
+  {
+    source.volume = 0f;
+    source.Play();
+  }
+
+  void Update()
+  {
+    UpdatePower(Input.GetAxis("Vertical"));
+    source.volume = power;
+    source.pitch = power * 3f;
+  }
+
+  private void UpdatePower(float input)
+  {
+    if (input == 0f)
+    {
+      power -= powerPerSecond * Time.deltaTime;
+    }
+    else
+    {
+      power += powerPerSecond * Time.deltaTime;
+    }
+    power = Mathf.Clamp01(power);
+  }
+}
